77ad92fc1ae33102bf41d983e8d18371c2e60649
berainwalia <berain.walia@gmail.com>
2026-07-30 00:12:34 +0530

    app.py

--- app.py
+++ app.py
@@ -250,7 +250,7 @@ tab1, tab3, tab5, tab10, tab15 = st.tabs(["1 Min", "3 Min", "5 Min", "10 Min", "
 
 for tab, mins in zip([tab1, tab3, tab5, tab10, tab15], [1, 3, 5, 10, 15]):
     with tab:
-        st.subheader(f"Top 10 Gainers - Last {mins} Minute(s)")
+        st.subheader(f"Top 10 Stocks - Last {mins} Minute(s)")
         df_gain, gain_col_name = calculate_gain(mins, now_str, segment_filter=selected_segment)
         
         if not df_gain.empty:
