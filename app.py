5ed64a9e8f0f7b220bc19bca909faa8b13b79d48
berainwalia <berain.walia@gmail.com>
2026-08-15 00:02:16 +0530

    app.py

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from datetime import datetime, timedelta, time
 import pytz
 from tradingview_screener import Query, Column
 from streamlit_autorefresh import st_autorefresh
+import threading
+import time as time_module
 
 # ==========================================
 # CONFIGURATION & SECTOR MAPPINGS
@@ -12,241 +14,90 @@ from streamlit_autorefresh import st_autorefresh
 DB_NAME = "relvol_fno_history.db"
 TIMEZONE = pytz.timezone("Asia/Kolkata")
 
-# Predefined Sector Mapping
 SECTOR_INDEX_MAP = {
-    "360ONE": "NIFTY Fin Service",
-    "ABB": "NIFTY Energy",
-    "ABBOTINDIA": "NIFTY Pharma",
-    "ABCAPITAL": "NIFTY Fin Service",
-    "ABSLAMC": "NIFTY Fin Service",
-    "ADANIENSOL": "NIFTY Energy",
-    "ADANIENT": "NIFTY Metal",
-    "ADANIGREEN": "NIFTY Infra",
-    "ADANIPORTS": "NIFTY Infra",
-    "ADANIPOWER": "NIFTY Energy",
-    "AEQUS": "NIFTY Defence",
-    "AJANTPHARM": "NIFTY Pharma",
-    "ALKEM": "NIFTY Healthcare",
-    "AMBER": "NIFTY Consumer Durables",
-    "AMBUJACEM": "NIFTY Infra",
-    "ANANDRATHI": "NIFTY Fin Service",
-    "ANGELONE": "NIFTY Fin Service",
-    "APLAPOLLO": "NIFTY Metal",
-    "APOLLOHOSP": "NIFTY Healthcare",
-    "ASHOKLEY": "NIFTY Auto",
-    "ASIANPAINT": "NIFTY Consumption",
-    "ASTRAL": "NIFTY Plastics",
-    "AUBANK": "NIFTY Bank",
-    "AUROPHARMA": "NIFTY Healthcare",
-    "AXISBANK": "NIFTY Bank",
-    "AXISCADES": "NIFTY Defence",
-    "BAJAJ-AUTO": "NIFTY Auto",
-    "BAJAJFINSV": "NIFTY Fin Service",
-    "BAJAJHLDNG": "NIFTY Fin Service",
-    "BAJFINANCE": "NIFTY Fin Service",
-    "BANDHANBNK": "NIFTY Bank",
-    "BANKBARODA": "NIFTY PSU Bank",
-    "BANKINDIA": "NIFTY PSU Bank",
-    "BDL": "NIFTY Defence",
-    "BEL": "NIFTY Defence",
-    "BHARATFORG": "NIFTY Auto",
-    "BHARTIARTL": "NIFTY Infra",
-    "BHEL": "NIFTY Energy",
-    "BIOCON": "NIFTY Healthcare",
-    "BLUESTARCO": "NIFTY Consumer Durables",
-    "BOSCHLTD": "NIFTY Auto",
-    "BPCL": "NIFTY Oil & Gas",
-    "BRITANNIA": "NIFTY FMCG",
-    "BSE": "NIFTY Fin Service",
-    "CAMS": "NIFTY Fin Service",
-    "CANBK": "NIFTY PSU Bank",
-    "CDSL": "NIFTY Fin Service",
-    "CGPOWER": "NIFTY Infra",
-    "CHOLAFIN": "NIFTY Fin Service",
-    "CIPLA": "NIFTY Healthcare",
-    "COALINDIA": "NIFTY Energy",
-    "COCHINSHIP": "NIFTY Defence",
-    "COFORGE": "NIFTY IT",
-    "COLPAL": "NIFTY FMCG",
-    "CONCOR": "NIFTY Transportation",
-    "CROMPTON": "NIFTY Consumer Durables",
-    "CUMMINSIN": "NIFTY Infra",
-    "CUMMINSIND": "NIFTY Infra",
-    "DABUR": "NIFTY FMCG",
-    "DALBHARAT": "NIFTY Infra",
-    "DELHIVERY": "NIFTY Transportation",
-    "DIVISLAB": "NIFTY Healthcare",
-    "DIXON": "NIFTY Consumer Durables",
-    "DLF": "NIFTY Realty",
-    "DMART": "NIFTY Consumer Durables",
-    "DRREDDY": "NIFTY Healthcare",
-    "EICHERMOT": "NIFTY Auto",
-    "ETERNAL": "NIFTY Services",
-    "EXIDEIND": "NIFTY Auto",
-    "FEDERALBNK": "NIFTY Bank",
-    "FORCEMOT": "NIFTY Auto",
-    "FORTIS": "NIFTY Healthcare",
-    "GAIL": "NIFTY Oil & Gas",
-    "GLAND": "NIFTY Pharma",
-    "GLENMARK": "NIFTY Healthcare",
-    "GMRAIRPORT": "NIFTY Infra",
-    "GODFRYPHLP": "NIFTY FMCG",
-    "GODREJCP": "NIFTY FMCG",
-    "GODREJPROP": "NIFTY Realty",
-    "GRASIM": "NIFTY Infra",
-    "GROWW": "NIFTY Fin Service",
-    "GVT&D": "NIFTY Energy",
-    "HAL": "NIFTY Defence",
-    "HAVELLS": "NIFTY Consumer Durables",
-    "HCLTECH": "NIFTY IT",
-    "HDFCAMC": "NIFTY Fin Service",
-    "HDFCBANK": "NIFTY Bank",
-    "HDFCLIFE": "NIFTY Fin Service",
-    "HEROMOTOCO": "NIFTY Auto",
-    "HINDALCO": "NIFTY Metal",
-    "HINDPETRO": "NIFTY Oil & Gas",
-    "HINDUNILVR": "NIFTY FMCG",
-    "HINDZINC": "NIFTY Metal",
-    "HYUNDAI": "NIFTY Auto",
-    "ICICIAMC": "NIFTY Fin Service",
-    "ICICIBANK": "NIFTY Bank",
-    "ICICIGI": "NIFTY Fin Service",
-    "ICICIPRULI": "NIFTY Fin Service",
-    "IDEA": "NIFTY Telecom",
-    "IDFCFIRSTB": "NIFTY Bank",
-    "IEX": "NIFTY Fin Service",
-    "INDHOTEL": "NIFTY Infra",
-    "INDIANB": "NIFTY PSU Bank",
-    "INDIGO": "NIFTY Infra",
-    "INDUSINDBK": "NIFTY Bank",
-    "INDUSTOWER": "NIFTY Infra",
-    "INFY": "NIFTY IT",
-    "INOXWIND": "NIFTY Energy",
-    "IOC": "NIFTY Oil & Gas",
-    "IPCALAB": "NIFTY Pharma",
-    "IREDA": "NIFTY Fin Service",
-    "IRFC": "NIFTY Fin Service",
-    "ITC": "NIFTY FMCG",
-    "JBCHEPHARM": "NIFTY Pharma",
-    "JINDALSTEL": "NIFTY Metal",
-    "JIOFIN": "NIFTY Fin Service",
-    "JSWENERGY": "NIFTY Energy",
-    "JSWSTEEL": "NIFTY Metal",
-    "JUBLFOOD": "NIFTY Consumer Durables",
-    "KALYANKJIL": "NIFTY Consumer Durables",
-    "KAYNES": "NIFTY Consumer Durables",
-    "KEI": "NIFTY Industrials",
-    "KFINTECH": "NIFTY Fin Service",
-    "KOTAKBANK": "NIFTY Bank",
-    "KPITTECH": "NIFTY IT",
-    "LAURUSLABS": "NIFTY Healthcare",
-    "LGEINDIA": "NIFTY Consumer Durables",
-    "LICHSGFIN": "NIFTY Fin Service",
-    "LICI": "NIFTY Fin Service",
-    "LODHA": "NIFTY Realty",
-    "LT": "NIFTY Infra",
-    "LTF": "NIFTY Fin Service",
-    "LTM": "NIFTY IT",
-    "LUPIN": "NIFTY Healthcare",
-    "M&M": "NIFTY Auto",
-    "MANAPPURAM": "NIFTY Fin Service",
-    "MANKIND": "NIFTY Healthcare",
-    "MARICO": "NIFTY FMCG",
-    "MARUTI": "NIFTY Auto",
-    "MAXHEALTH": "NIFTY Healthcare",
-    "MAZDOCK": "NIFTY Defence",
-    "MCX": "NIFTY Fin Service",
-    "MFSL": "NIFTY Fin Service",
-    "MOTHERSON": "NIFTY Auto",
-    "MOTILALOFS": "NIFTY Fin Service",
-    "MPHASIS": "NIFTY IT",
-    "MUTHOOTFIN": "NIFTY Fin Service",
-    "NAM-INDIA": "NIFTY Fin Service",
-    "NATIONALUM": "NIFTY Metal",
-    "NAUKRI": "NIFTY IT",
-    "NBCC": "NIFTY Realty",
-    "NESTLEIND": "NIFTY FMCG",
-    "NHPC": "NIFTY Energy",
-    "NMDC": "NIFTY Metal",
-    "NTPC": "NIFTY Infra",
-    "NUVAMA": "NIFTY Fin Service",
-    "NYKAA": "NIFTY IT",
-    "OBEROIRLTY": "NIFTY Realty",
-    "OFSS": "NIFTY IT",
-    "OIL": "NIFTY Oil & Gas",
-    "ONGC": "NIFTY Oil & Gas",
-    "PAGEIND": "NIFTY Consumer Durables",
-    "PATANJALI": "NIFTY FMCG",
-    "PAYTM": "NIFTY Fin Service",
-    "PERSISTENT": "NIFTY IT",
-    "PETRONET": "NIFTY Oil & Gas",
-    "PFC": "NIFTY Fin Service",
-    "PGEL": "NIFTY Consumer Durables",
-    "PHOENIXLTD": "NIFTY Realty",
-    "PIDILITIND": "NIFTY Chemicals",
-    "PIIND": "NIFTY Chemicals",
-    "PNB": "NIFTY PSU Bank",
-    "PNBHOUSING": "NIFTY Fin Service",
-    "POLICYBZR": "NIFTY Fin Service",
-    "POLYCAB": "NIFTY Industrials",
-    "POWERGRID": "NIFTY Infra",
-    "POWERINDIA": "NIFTY Energy",
-    "PREMIERENE": "NIFTY Services",
-    "PRESTIGE": "NIFTY Realty",
-    "RADICO": "NIFTY FMCG",
-    "RBLBANK": "NIFTY Bank",
-    "RECLTD": "NIFTY Fin Service",
-    "RELIANCE": "NIFTY Oil & Gas",
-    "RVNL": "NIFTY Realty",
-    "SAIL": "NIFTY Metal",
-    "SBICARD": "NIFTY Fin Service",
-    "SBILIFE": "NIFTY Fin Service",
-    "SBIN": "NIFTY PSU Bank",
-    "SHREECEM": "NIFTY Infra",
-    "SHRIRAMFIN": "NIFTY Fin Service",
-    "SIEMENS": "NIFTY Energy",
-    "SOLARINDS": "NIFTY Defence",
-    "SONACOMS": "NIFTY Auto",
-    "SRF": "NIFTY Chemicals",
-    "SUNPHARMA": "NIFTY Healthcare",
-    "SUPREMEIND": "NIFTY Plastics",
-    "SUZLON": "NIFTY Infra",
-    "SWIGGY": "NIFTY Services",
-    "TATACONSUM": "NIFTY FMCG",
-    "TATAELXSI": "NIFTY IT",
-    "TATAPOWER": "NIFTY Infra",
-    "TATASTEEL": "NIFTY Metal",
-    "TCS": "NIFTY IT",
-    "TECHM": "NIFTY IT",
-    "TIINDIA": "NIFTY Auto",
-    "TITAN": "NIFTY Consumer Durables",
-    "TMPV": "NIFTY Auto",
-    "TORNTPHARM": "NIFTY Healthcare",
-    "TRENT": "NIFTY Consumer Durables",
-    "TVSMOTOR": "NIFTY Auto",
-    "ULTRACEMCO": "NIFTY Infra",
-    "UNIMECH": "NIFTY Defence",
-    "UNIONBANK": "NIFTY PSU Bank",
-    "UNITDSPR": "NIFTY FMCG",
-    "UNOMINDA": "NIFTY Auto",
-    "UPL": "NIFTY Chemicals",
-    "UTIAMC": "NIFTY Fin Service",
-    "VBL": "NIFTY FMCG",
-    "VEDL": "NIFTY Metal",
-    "VMM": "NIFTY Consumer Durables",
-    "VOLTAS": "NIFTY Consumer Durables",
-    "WAAREEENER": "NIFTY Industrials",
-    "WIPRO": "NIFTY IT",
-    "WOCKPHARMA": "NIFTY Pharma",
-    "YESBANK": "NIFTY Bank",
-    "ZYDUSLIFE": "NIFTY Healthcare",
+    "360ONE": "NIFTY Fin Service", "ABB": "NIFTY Energy", "ABBOTINDIA": "NIFTY Pharma",
+    "ABCAPITAL": "NIFTY Fin Service", "ABSLAMC": "NIFTY Fin Service", "ADANIENSOL": "NIFTY Energy",
+    "ADANIENT": "NIFTY Metal", "ADANIGREEN": "NIFTY Infra", "ADANIPORTS": "NIFTY Infra",
+    "ADANIPOWER": "NIFTY Energy", "AEQUS": "NIFTY Defence", "AJANTPHARM": "NIFTY Pharma",
+    "ALKEM": "NIFTY Healthcare", "AMBER": "NIFTY Consumer Durables", "AMBUJACEM": "NIFTY Infra",
+    "ANANDRATHI": "NIFTY Fin Service", "ANGELONE": "NIFTY Fin Service", "APLAPOLLO": "NIFTY Metal",
+    "APOLLOHOSP": "NIFTY Healthcare", "ASHOKLEY": "NIFTY Auto", "ASIANPAINT": "NIFTY Consumption",
+    "ASTRAL": "NIFTY Plastics", "AUBANK": "NIFTY Bank", "AUROPHARMA": "NIFTY Healthcare",
+    "AXISBANK": "NIFTY Bank", "AXISCADES": "NIFTY Defence", "BAJAJ-AUTO": "NIFTY Auto",
+    "BAJAJFINSV": "NIFTY Fin Service", "BAJAJHLDNG": "NIFTY Fin Service", "BAJFINANCE": "NIFTY Fin Service",
+    "BANDHANBNK": "NIFTY Bank", "BANKBARODA": "NIFTY PSU Bank", "BANKINDIA": "NIFTY PSU Bank",
+    "BDL": "NIFTY Defence", "BEL": "NIFTY Defence", "BHARATFORG": "NIFTY Auto",
+    "BHARTIARTL": "NIFTY Infra", "BHEL": "NIFTY Energy", "BIOCON": "NIFTY Healthcare",
+    "BLUESTARCO": "NIFTY Consumer Durables", "BOSCHLTD": "NIFTY Auto", "BPCL": "NIFTY Oil & Gas",
+    "BRITANNIA": "NIFTY FMCG", "BSE": "NIFTY Fin Service", "CAMS": "NIFTY Fin Service",
+    "CANBK": "NIFTY PSU Bank", "CDSL": "NIFTY Fin Service", "CGPOWER": "NIFTY Infra",
+    "CHOLAFIN": "NIFTY Fin Service", "CIPLA": "NIFTY Healthcare", "COALINDIA": "NIFTY Energy",
+    "COCHINSHIP": "NIFTY Defence", "COFORGE": "NIFTY IT", "COLPAL": "NIFTY FMCG",
+    "CONCOR": "NIFTY Transportation", "CROMPTON": "NIFTY Consumer Durables", "CUMMINSIN": "NIFTY Infra",
+    "CUMMINSIND": "NIFTY Infra", "DABUR": "NIFTY FMCG", "DALBHARAT": "NIFTY Infra",
+    "DELHIVERY": "NIFTY Transportation", "DIVISLAB": "NIFTY Healthcare", "DIXON": "NIFTY Consumer Durables",
+    "DLF": "NIFTY Realty", "DMART": "NIFTY Consumer Durables", "DRREDDY": "NIFTY Healthcare",
+    "EICHERMOT": "NIFTY Auto", "ETERNAL": "NIFTY Services", "EXIDEIND": "NIFTY Auto",
+    "FEDERALBNK": "NIFTY Bank", "FORCEMOT": "NIFTY Auto", "FORTIS": "NIFTY Healthcare",
+    "GAIL": "NIFTY Oil & Gas", "GLAND": "NIFTY Pharma", "GLENMARK": "NIFTY Healthcare",
+    "GMRAIRPORT": "NIFTY Infra", "GODFRYPHLP": "NIFTY FMCG", "GODREJCP": "NIFTY FMCG",
+    "GODREJPROP": "NIFTY Realty", "GRASIM": "NIFTY Infra", "GROWW": "NIFTY Fin Service",
+    "GVT&D": "NIFTY Energy", "HAL": "NIFTY Defence", "HAVELLS": "NIFTY Consumer Durables",
+    "HCLTECH": "NIFTY IT", "HDFCAMC": "NIFTY Fin Service", "HDFCBANK": "NIFTY Bank",
+    "HDFCLIFE": "NIFTY Fin Service", "HEROMOTOCO": "NIFTY Auto", "HINDALCO": "NIFTY Metal",
+    "HINDPETRO": "NIFTY Oil & Gas", "HINDUNILVR": "NIFTY FMCG", "HINDZINC": "NIFTY Metal",
+    "HYUNDAI": "NIFTY Auto", "ICICIAMC": "NIFTY Fin Service", "ICICIBANK": "NIFTY Bank",
+    "ICICIGI": "NIFTY Fin Service", "ICICIPRULI": "NIFTY Fin Service", "IDEA": "NIFTY Telecom",
+    "IDFCFIRSTB": "NIFTY Bank", "IEX": "NIFTY Fin Service", "INDHOTEL": "NIFTY Infra",
+    "INDIANB": "NIFTY PSU Bank", "INDIGO": "NIFTY Infra", "INDUSINDBK": "NIFTY Bank",
+    "INDUSTOWER": "NIFTY Infra", "INFY": "NIFTY IT", "INOXWIND": "NIFTY Energy",
+    "IOC": "NIFTY Oil & Gas", "IPCALAB": "NIFTY Pharma", "IREDA": "NIFTY Fin Service",
+    "IRFC": "NIFTY Fin Service", "ITC": "NIFTY FMCG", "JBCHEPHARM": "NIFTY Pharma",
+    "JINDALSTEL": "NIFTY Metal", "JIOFIN": "NIFTY Fin Service", "JSWENERGY": "NIFTY Energy",
+    "JSWSTEEL": "NIFTY Metal", "JUBLFOOD": "NIFTY Consumer Durables", "KALYANKJIL": "NIFTY Consumer Durables",
+    "KAYNES": "NIFTY Consumer Durables", "KEI": "NIFTY Industrials", "KFINTECH": "NIFTY Fin Service",
+    "KOTAKBANK": "NIFTY Bank", "KPITTECH": "NIFTY IT", "LAURUSLABS": "NIFTY Healthcare",
+    "LGEINDIA": "NIFTY Consumer Durables", "LICHSGFIN": "NIFTY Fin Service", "LICI": "NIFTY Fin Service",
+    "LODHA": "NIFTY Realty", "LT": "NIFTY Infra", "LTF": "NIFTY Fin Service",
+    "LTM": "NIFTY IT", "LUPIN": "NIFTY Healthcare", "M&M": "NIFTY Auto",
+    "MANAPPURAM": "NIFTY Fin Service", "MANKIND": "NIFTY Healthcare", "MARICO": "NIFTY FMCG",
+    "MARUTI": "NIFTY Auto", "MAXHEALTH": "NIFTY Healthcare", "MAZDOCK": "NIFTY Defence",
+    "MCX": "NIFTY Fin Service", "MFSL": "NIFTY Fin Service", "MOTHERSON": "NIFTY Auto",
+    "MOTILALOFS": "NIFTY Fin Service", "MPHASIS": "NIFTY IT", "MUTHOOTFIN": "NIFTY Fin Service",
+    "NAM-INDIA": "NIFTY Fin Service", "NATIONALUM": "NIFTY Metal", "NAUKRI": "NIFTY IT",
+    "NBCC": "NIFTY Realty", "NESTLEIND": "NIFTY FMCG", "NHPC": "NIFTY Energy",
+    "NMDC": "NIFTY Metal", "NTPC": "NIFTY Infra", "NUVAMA": "NIFTY Fin Service",
+    "NYKAA": "NIFTY IT", "OBEROIRLTY": "NIFTY Realty", "OFSS": "NIFTY IT",
+    "OIL": "NIFTY Oil & Gas", "ONGC": "NIFTY Oil & Gas", "PAGEIND": "NIFTY Consumer Durables",
+    "PATANJALI": "NIFTY FMCG", "PAYTM": "NIFTY Fin Service", "PERSISTENT": "NIFTY IT",
+    "PETRONET": "NIFTY Oil & Gas", "PFC": "NIFTY Fin Service", "PGEL": "NIFTY Consumer Durables",
+    "PHOENIXLTD": "NIFTY Realty", "PIDILITIND": "NIFTY Chemicals", "PIIND": "NIFTY Chemicals",
+    "PNB": "NIFTY PSU Bank", "PNBHOUSING": "NIFTY Fin Service", "POLICYBZR": "NIFTY Fin Service",
+    "POLYCAB": "NIFTY Industrials", "POWERGRID": "NIFTY Infra", "POWERINDIA": "NIFTY Energy",
+    "PREMIERENE": "NIFTY Services", "PRESTIGE": "NIFTY Realty", "RADICO": "NIFTY FMCG",
+    "RBLBANK": "NIFTY Bank", "RECLTD": "NIFTY Fin Service", "RELIANCE": "NIFTY Oil & Gas",
+    "RVNL": "NIFTY Realty", "SAIL": "NIFTY Metal", "SBICARD": "NIFTY Fin Service",
+    "SBILIFE": "NIFTY Fin Service", "SBIN": "NIFTY PSU Bank", "SHREECEM": "NIFTY Infra",
+    "SHRIRAMFIN": "NIFTY Fin Service", "SIEMENS": "NIFTY Energy", "SOLARINDS": "NIFTY Defence",
+    "SONACOMS": "NIFTY Auto", "SRF": "NIFTY Chemicals", "SUNPHARMA": "NIFTY Healthcare",
+    "SUPREMEIND": "NIFTY Plastics", "SUZLON": "NIFTY Infra", "SWIGGY": "NIFTY Services",
+    "TATACONSUM": "NIFTY FMCG", "TATAELXSI": "NIFTY IT", "TATAPOWER": "NIFTY Infra",
+    "TATASTEEL": "NIFTY Metal", "TCS": "NIFTY IT", "TECHM": "NIFTY IT",
+    "TIINDIA": "NIFTY Auto", "TITAN": "NIFTY Consumer Durables", "TMPV": "NIFTY Auto",
+    "TORNTPHARM": "NIFTY Healthcare", "TRENT": "NIFTY Consumer Durables", "TVSMOTOR": "NIFTY Auto",
+    "ULTRACEMCO": "NIFTY Infra", "UNIMECH": "NIFTY Defence", "UNIONBANK": "NIFTY PSU Bank",
+    "UNITDSPR": "NIFTY FMCG", "UNOMINDA": "NIFTY Auto", "UPL": "NIFTY Chemicals",
+    "UTIAMC": "NIFTY Fin Service", "VBL": "NIFTY FMCG", "VEDL": "NIFTY Metal",
+    "VMM": "NIFTY Consumer Durables", "VOLTAS": "NIFTY Consumer Durables", "WAAREEENER": "NIFTY Industrials",
+    "WIPRO": "NIFTY IT", "WOCKPHARMA": "NIFTY Pharma", "YESBANK": "NIFTY Bank",
+    "ZYDUSLIFE": "NIFTY Healthcare"
 }
 
 VALID_SYMBOLS = set(SECTOR_INDEX_MAP.keys())
 
 st.set_page_config(page_title="NSE Relative Volume Tracker", layout="wide")
 
-# Auto-refresh every 60 seconds
+# UI Auto-refresh every 60 seconds
 st_autorefresh(interval=60000, key="datarefresh")
 
 # ==========================================
@@ -305,10 +156,8 @@ def save_snapshot(df, now_str):
 # ==========================================
 # DYNAMIC SCANNER FETCHING
 # ==========================================
-@st.cache_data(ttl=300)
 def fetch_live_fno_data(stock_universe_mode="Custom List"):
     try:
-        # Determine query limit based on selected mode
         limit = 200 if stock_universe_mode == "Custom List" else (250 if stock_universe_mode == "All F&O Stocks" else 500)
         
         df = (
@@ -329,21 +178,54 @@ def fetch_live_fno_data(stock_universe_mode="Custom List"):
         
         df['Symbol'] = df['Symbol'].astype(str).str.upper().str.strip()
         
-        # Filter symbols dynamically according to the user selection
         if stock_universe_mode == "Custom List":
             df = df[df['Symbol'].isin(VALID_SYMBOLS)].copy()
         
         df['RelVol'] = pd.to_numeric(df['RelVol'], errors='coerce')
         df['ChangePct'] = pd.to_numeric(df['ChangePct'], errors='coerce')
         
-        # Sector Mapping with TradingView Fallback
         df['Sector Index'] = df['Symbol'].map(SECTOR_INDEX_MAP).fillna(df['TV Sector'].fillna("Other Sector"))
         
         return df.dropna(subset=['RelVol', 'ChangePct']).reset_index(drop=True)
 
     except Exception as e:
-        st.error(f"Error pulling stock updates: {e}")
+        print(f"Error pulling stock updates: {e}")
         return pd.DataFrame()
+
+# ==========================================
+# BACKGROUND AUTOMATIC SNAPSHOT SCHEDULER
+# ==========================================
+def auto_snapshot_loop():
+    """Takes background snapshots every 60 seconds independent of web UI status."""
+    while True:
+        try:
+            now_dt = datetime.now(TIMEZONE)
+            # Market days: Monday (0) to Friday (4)
+            if now_dt.weekday() < 5:
+                curr_time = now_dt.time()
+                # NSE Market Hours: 09:15 AM to 03:30 PM IST
+                if time(9, 15) <= curr_time <= time(15, 30):
+                    now_str = now_dt.strftime("%Y-%m-%d %H:%M:%S")
+                    today_date_str = now_dt.strftime("%Y-%m-%d")
+                    
+                    check_and_reset_daily(today_date_str)
+                    
+                    # Fetch and save live background snapshot
+                    df = fetch_live_fno_data("All F&O Stocks")
+                    if not df.empty:
+                        save_snapshot(df, now_str)
+        except Exception as e:
+            print(f"Background Snapshot Exception: {e}")
+            
+        time_module.sleep(60)
+
+# Initialize DB & Start Background Worker Thread (Only Once)
+init_db()
+
+if "bg_thread_started" not in st.session_state:
+    st.session_state["bg_thread_started"] = True
+    bg_thread = threading.Thread(target=auto_snapshot_loop, daemon=True)
+    bg_thread.start()
 
 # ==========================================
 # CALCULATIONS & PROCESSING
@@ -533,15 +415,11 @@ def generate_5min_time_options():
     return time_options
 
 # ==========================================
-# DASHBOARD INITIALIZATION & UI
+# DASHBOARD UI
 # ==========================================
-init_db()
-
 now_dt = datetime.now(TIMEZONE)
 now_str = now_dt.strftime("%Y-%m-%d %H:%M:%S")
 today_date_str = now_dt.strftime("%Y-%m-%d")
-
-check_and_reset_daily(today_date_str)
 
 # Sidebar Configuration
 st.sidebar.header("📌 Stock Universe Selection")
@@ -585,13 +463,11 @@ if st.sidebar.button("🧹 Clear Snapshot History"):
     st.sidebar.success("Database reset successful!")
     st.rerun()
 
-# Fetch Dynamic Screener Data
+# Fetch UI Data
 live_df = fetch_live_fno_data(stock_universe_mode)
-if not live_df.empty:
-    save_snapshot(live_df, now_str)
 
 st.title("⚡ NSE Relative Volume & Price Movers")
-st.caption(f"Active Stock Universe: **{stock_universe_mode} ({len(live_df)} Active Tickers)** | Last updated: {now_str} IST (Auto-refreshes every 60s)")
+st.caption(f"Active Universe: **{stock_universe_mode} ({len(live_df)} Tickers)** | Background Worker Status: 🟢 **Active (1m Auto-Snapshots)** | Last refreshed: {now_str} IST")
 
 # Timeframe Tabs
 tab1, tab3, tab5, tab10, tab15, tab_custom, tab_day, tab_sector = st.tabs([
@@ -618,7 +494,7 @@ for tab, mins in zip([tab1, tab3, tab5, tab10, tab15], [1, 3, 5, 10, 15]):
                 }
             )
         else:
-            st.info("Accumulating intraday snapshot data... Please wait a few moments.")
+            st.info("Accumulating minute-by-minute background snapshots... Please wait a few moments.")
 
 # Custom Range Tab
 with tab_custom:
